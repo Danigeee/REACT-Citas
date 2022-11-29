c7d73d039d034cb57e05dda4fd86d3da79a6fc99
Danigee <daniav9924@gmail.com>
2022-11-28 22:43:00 -0500

    validacion

--- src/components/AppForm.jsx
+++ src/components/AppForm.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {useState, useEffect} from "react"
 
 const AppForm = () => {
+  const [nombre, setNombre] = useState("");
+  const [propietario, setPropietario] = useState("");
+  const [email, setEmail] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [sintomas, setSintomas] = useState("");
+const [error, setError] = useState(false)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    //validacion del formulario
+    if([nombre,propietario,email,fecha,sintomas].includes("")){
+      setError(true)
+    }else{
+      console.log("todob ein");
+    }
+  }
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
      <h2 className="font-black text-3xl text-center">
       Seguimiento Pacientes
       </h2>
@@ -12,8 +31,15 @@ const AppForm = () => {
       <samp className='text-indigo-600 font-bold'>Administralos</samp>
       </p>
 
-      <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
-
+      <form 
+        onSubmit={handleSubmit}
+        className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'
+      >
+        {error &&(
+          <div className='bg-red-800 text-white text-center p-3 uppercase font-bold rounded mb-3'> 
+           <p>Todos los campos son obligatorios</p>
+          </div>
+        )}
       <div className='mb-5'>
         <label htmlFor='mascota' className=' block text-gray-700 uppercase font-bold '>Nombre Mascota</label>
         <input
@@ -21,16 +47,20 @@ const AppForm = () => {
           className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
           type="text" 
           placeholder='Nombre de la mascota'
+          value={nombre}
+          onChange={(e)=> setNombre(e.target.value)}
          />
       </div>
 
       <div className='mb-5'>
-        <label htmlFor='Propietario' className=' block text-gray-700 uppercase font-bold '>Nombre Propietario</label>
+        <label htmlFor='propietario' className=' block text-gray-700 uppercase font-bold '>Nombre Propietario</label>
         <input
-          id='Propietario'
+          id='propietario'
           className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
           type="text" 
-          placeholder='Nombre dle Propietario'
+          placeholder='Nombre dle propietario'
+          value={propietario}
+          onChange={(e)=> setPropietario(e.target.value)}
          />
       </div>
 
@@ -41,6 +71,8 @@ const AppForm = () => {
           className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
           type="email" 
           placeholder='Nombre de la mascota'
+          value={email}
+          onChange={(e)=> setEmail(e.target.value)}
          />
       </div>
 
@@ -50,7 +82,9 @@ const AppForm = () => {
           id='alta'
           className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
           type="date" 
-          placeholder='Nombre dle Propietario'
+          placeholder='Nombre dle Alta'
+          value={fecha}
+          onChange={(e)=> setFecha(e.target.value)}
          />
       </div>
 
@@ -60,6 +94,8 @@ const AppForm = () => {
          id="sintomas"  
          className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md '
          placeholder='Describe los sintomas'
+         value={sintomas}
+         onChange={(e)=> setSintomas(e.target.value)}
          />
       </div>
 
